2c3e7bfea1470322ec40d3ca5b49a56e5c11067f
tekochae <christian.haefliger@edu.teko.ch>
2021-05-17 21:27:08 +0200

    Bereinigung

--- app/src/main/java/com/example/pizzaapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaapp/MainActivity.kt
@@ -7,17 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //this ist my first test
-        //gfjhsf
-        //ljkgbg
-        //Hallo
-        // Hallo zurück
-        // Test Chregu 1
-        // Test Luca
-        // Test Chregu 2
-        // Test Luca 2
-        // Test Simon
-        // Test 0815
     }
 }
