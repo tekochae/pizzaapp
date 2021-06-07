ce3bb17fd275ea8023f1bec502cc58daaeee73dd
straescher <simi_tresch@hotmail.com>
2021-06-07 20:23:16 +0200

    choosegui

---
+++ app/src/main/java/com/example/pizzaapp/ChooseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pizzaapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChooseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.choose)
+    }
+}
