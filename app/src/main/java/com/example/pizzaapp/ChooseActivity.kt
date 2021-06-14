b31490f6321a848d7e4f64eb11f2ddfe29de4696
straescher <simi_tresch@hotmail.com>
2021-06-14 19:29:41 +0200

    main onlick choose, dropdown auf choose

--- app/src/main/java/com/example/pizzaapp/ChooseActivity.kt
+++ app/src/main/java/com/example/pizzaapp/ChooseActivity.kt
@@ -1,11 +1,52 @@
 package com.example.pizzaapp
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class ChooseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.choose)
+
+        val spinnertyp: Spinner = findViewById(R.id.choosepizzatyp);
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.choosepizzatyp,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnertyp.adapter = adapter
+        }
+
+        val spinneranzahl: Spinner = findViewById(R.id.choosepizzaanzahl);
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.choosepizzaanzahl,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinneranzahl.adapter = adapter
+        }
+
+        val spinnergroesse: Spinner = findViewById(R.id.choosepizzagroesse);
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.choosepizzagroesse,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnergroesse.adapter = adapter
+        }
     }
 }
--- app/src/main/java/com/example/pizzaapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.pizzaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onclick (view: View) {
+        val intent = Intent(this, ChooseActivity::class.java)
+        startActivity(intent)
     }
 }
