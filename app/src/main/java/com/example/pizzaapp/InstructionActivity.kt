65a211e0a9c6372d5afce3ccd1e29079569bfc9a
straescher <simi_tresch@hotmail.com>
2021-08-30 18:32:43 +0200

    30.08.2021: Push 1. Stand

--- app/src/main/java/com/example/pizzaapp/InstructionActivity.kt
+++ app/src/main/java/com/example/pizzaapp/InstructionActivity.kt
@@ -3,6 +3,7 @@ package com.example.pizzaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 
 class InstructionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,4 +11,15 @@ class InstructionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_instruction)
     }
 }
+/*    fun startTimer(message: String, seconds: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, "Test Timer")
+            putExtra(AlarmClock.EXTRA_LENGTH, seconds)
+            putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }*/
+
 
--- app/src/main/java/com/example/pizzaapp/ResultActivity.kt
+++ app/src/main/java/com/example/pizzaapp/ResultActivity.kt
@@ -3,13 +3,12 @@ package com.example.pizzaapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import com.example.pizzaapp.domain.Pizzateig
 
 class ResultActivity : AppCompatActivity() {
-
-
-    //lateinit var spinnerfermentierung: Spinner
+    lateinit var spinnerfermentierung: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +18,16 @@ class ResultActivity : AppCompatActivity() {
         val anzeige = findViewById<TextView>(R.id.AnzeigeBerechnungZutaten)
         anzeige.text = pizzateig.anzeigeBerechnung()
 
-//        spinnerfermentierung = findViewById(R.id.choosefermentierung);
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.choosefermentierung,
-//            android.R.layout.simple_spinner_item
-//        ).also { adapter ->
-//            // Specify the layout to use when the list of choices appears
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            // Apply the adapter to the spinner
-//            spinnerfermentierung.adapter = adapter
-//        }
+        spinnerfermentierung = findViewById(R.id.choosefermentierung);
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.choosefermentierung,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerfermentierung.adapter = adapter
+        }
     }
 }
